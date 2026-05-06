e0f95e4e2b6438656ee96c64471754698423c118
aya-k-o <huwa1019.1205@gmail.com>
2026-05-06 21:48:48 +0900

    Add favicon to admin and index pages

--- admin.php
+++ admin.php
@@ -17,6 +17,7 @@ if (!isset($_SESSION['admin_logged_in'])) {
           <meta charset="UTF-8">
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
           <link rel="stylesheet" href="style.css">
+          <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🧹</text></svg>">
           <title>管理画面 ログイン</title>
         </head>
         <body>
@@ -93,6 +94,7 @@ $locations = $pdo->query("SELECT * FROM locations")->fetchAll();
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="style.css">
+   <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🧹</text></svg>">
   <title>管理画面</title>
 </head>
 <body>
